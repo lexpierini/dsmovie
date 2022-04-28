3e3857bb816bd0648de7b4534b67df60afc770e0
lexpierin <alex_pierini@hotmail.com>
2022-04-27 23:10:14 -0400

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+  // Mauvais façon
+  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((resp) => {
+    console.log(resp.data);
+  });
+
   return (
     <>
       <Pagination />
